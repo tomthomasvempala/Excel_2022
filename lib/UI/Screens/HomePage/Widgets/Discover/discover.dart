69174d80c8ba767c90cb6869bb7bd0c868ffa77a
Tom Vempala <thommuvtt123@gmail.com>
2022-10-08 13:58:13 +0530

    added widgets to work on

---
+++ lib/UI/Screens/HomePage/Widgets/Discover/discover.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Discover extends StatelessWidget {
+  const Discover({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
