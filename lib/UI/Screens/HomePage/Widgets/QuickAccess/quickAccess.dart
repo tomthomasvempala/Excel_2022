49a8cf021c9b9656c1fc592f981f2ff243e9b007
Tom Vempala <thommuvtt123@gmail.com>
2022-10-08 14:50:38 +0530

    quick acces empty widget for faheem

---
+++ lib/UI/Screens/HomePage/Widgets/QuickAccess/quickAccess.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class QuickAccessBar extends StatelessWidget {
+  const QuickAccessBar({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
