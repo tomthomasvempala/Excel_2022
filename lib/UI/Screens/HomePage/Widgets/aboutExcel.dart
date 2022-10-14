a5a4649e00eef38071efcdc56739aa1d4fcf800e
Pooja-Johnson <poojajohnson2002@gmail.com>
2022-10-14 07:40:23 +0530

    changes in aboutExcel

--- lib/UI/Screens/HomePage/Widgets/aboutExcel.dart
+++ lib/UI/Screens/HomePage/Widgets/aboutExcel.dart
@@ -47,19 +47,72 @@ class AboutExcelPopUpState extends State<AboutExcelPopUp>
                 ),
                 SizedBox(height: 10),
                 Center(
-                  child: Text(
-                    "Excel, the nation’s second and South India’s first ever fest of its kind, was started in 2001 by the students of Govt. Model Engineering College. Over the years, Excel has grown exponentially, consistently playing host to some of the most talented students, the most illustrious speakers and the most reputed companies. Now gearing towards the landmark 21st edition, Excel continues to excite the participants with its wide array of technical and non-technical events and with a promise to live up to its motto. Excel 2020 incorporates a wide variety of technical, managerial and general events which includes competitions, workshops, summits, hackathons, exhibitions and so on.\n\n Join us on 29th, 30th and 31st January 2021 to experience the magic of Excel.",
-                    style: TextStyle(
-                      color: Color(0xff555566),
-                      fontSize: 13,
-                      fontFamily: pfontFamily,
-                      height: 1.3,
-                    ),
-                    textAlign: TextAlign.center,
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Inspire. Innovate. Engineer ',
+                      style: TextStyle(
+                        color: Color(0xDD000000),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                        fontFamily: pfontFamily,
+                        height: 1.3,),
+
+                      children: const <TextSpan>[
+                        TextSpan(text: "\nExcel, the nation’s second and South India’s first ever fest of its kind, was started in 2001 by the students of Govt. Model Engineering College. Over the years, Excel has grown exponentially, consistently playing host to some of the most talented students, the most illustrious speakers and the most reputed companies. \n\n Now gearing towards the landmark 23rd edition, Excel continues to march forward. Join us this December to experience the magic of Excel!.",
+                          style: TextStyle(
+                          color: Color(0xff555566),
+                            fontWeight: FontWeight.normal,
+                          fontSize: 13,
+                          fontFamily: pfontFamily,
+                          height: 1.3,
+                        ),),
+                      ],
+
                   ),
+                  ),
+
                 ),
                 SizedBox(height: 20),
-                Row(
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Center(
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(40)
+                              )
+                            ),
+                            foregroundColor: getColor(Colors.white, Colors.blue),
+                            backgroundColor: getColor(Colors.blue, Colors.white),
+                            //overlayColor: getColor(Colors.white, Colors.teal),
+                          ),
+                          child: Text('View Website '),
+                          onPressed: () {},
+                        )
+                    ),
+                    Center(
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)
+                                )
+                            ),
+                            foregroundColor: getColor(Colors.white, Colors.blue),
+                            backgroundColor: getColor(Colors.blue, Colors.white),
+                            //overlayColor: getColor(Colors.white, Colors.teal),
+                          ),
+                          child: Text('Developer Credits '),
+                          onPressed: () {},
+                        )
+                    )
+                  ],
+                )
+                /*Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Image.asset(
@@ -71,12 +124,23 @@ class AboutExcelPopUpState extends State<AboutExcelPopUp>
                       height: 50,
                     ),
                   ],
-                ),
+                )*/,
               ],
             ),
           ),
         ),
       ),
+
     );
+
   }
+
 }
+MaterialStateProperty<Color> getColor(Color color, Color colorPressed){
+
+  final getColor = (Set<MaterialState> states) {
+    if (states.contains(MaterialState.pressed)) {
+      return colorPressed;
+    }
+  };
+  return MaterialStateProperty.resolveWith(getColor);}
