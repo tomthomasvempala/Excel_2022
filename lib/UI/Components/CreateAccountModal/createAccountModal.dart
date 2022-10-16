065154e191f104c5c6a947543b0a7a7d3bbc4048
Tom Vempala <thommuvtt123@gmail.com>
2022-10-16 15:27:52 +0530

    sthg

--- lib/UI/Components/CreateAccountModal/createAccountModal.dart
+++ lib/UI/Components/CreateAccountModal/createAccountModal.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
-void showCreateAccountModal(context){
+void showCreateAccountModal(context) {
   showModalBottomSheet<dynamic>(
-                        isScrollControlled: true,
-                        useRootNavigator: true,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(40),
-                                topRight: Radius.circular(40))),
-                        constraints: BoxConstraints(
-                          minWidth: MediaQuery.of(context).size.width,
-                        ),
-                        context: context,
-                        builder: (context) =>
-                            Wrap(children: <Widget>[]));
+      isScrollControlled: true,
+      useRootNavigator: true,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40), topRight: Radius.circular(40))),
+      constraints: BoxConstraints(
+        minWidth: MediaQuery.of(context).size.width,
+      ),
+      context: context,
+      builder: (context) => Wrap(children: <Widget>[CreateAccountModal()]));
 }
 
+class CreateAccountModal extends StatelessWidget {
+  const CreateAccountModal({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
