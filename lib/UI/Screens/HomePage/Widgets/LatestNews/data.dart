0bc53609144f6799897ae71e69aabfbe02102788
jaison080 <jaisondennis080@gmail.com>
2022-12-19 20:57:08 +0530

    Fixed Eccentricity Issue and also Updated Figma Links of News

--- lib/UI/Screens/HomePage/Widgets/LatestNews/data.dart
+++ lib/UI/Screens/HomePage/Widgets/LatestNews/data.dart
@@ -7,7 +7,7 @@ var NewsData = [
     desc:
         "It's time to finally launch the journey we've all planned together! Excel 2021 is here with new stuf..",
     image:
-        'https://s3-alpha-sig.figma.com/img/5377/d83f/0f982e758c42488116079c3d1907709b?Expires=1666569600&Signature=hqtodI7r6DeDm5Gq8H1K0hq~~A9xYN24hdIkC~Ra2PbQejatsKfQRJgE73v9iOYSQ~0Qf3NR0F71Zsbqpf2-q1ILDQAmbopM1iBtsdMfPi8u7uZ86cexmBu0K-iqbq5pz8IyeZlb52qXWDVaC87hNFuhcUgGPMmDPjiU5UEerdLrGF5JGmZ2rjzELltPoXpW7qqLgcP4iReq1T6HHFjkKmLT7mour1rz7g4tmhPFwKhO583QS50x-POIyp8wC7sLExE6YI1seXXup8eC1U5JtSQ0~Ka0DcIsxYRY4mh9v8G37vazaxk2bOitfbiSMHnAEBGyb~WcwzSBPjtPMzMEtQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+        'https://s3-alpha-sig.figma.com/img/5377/d83f/0f982e758c42488116079c3d1907709b?Expires=1672617600&Signature=W-dyWHcUOEgaY5wSgdhYcNlcqSBCumyxQbbr9OLiX4Vy8znGkocf-GXHudLG0rg9QNzgsZrAgC~5JXPkdlknPyzbkpDK2GHSxxVsma6dT8UHev0vrClqfIA0aoWjJ1ikV8u46FEl2UeZ2gi0I9s8A-ku0hEO47szvBodi2nGtELmXCyPRQWS61RzeMXL6CaZR7utEs7nCBrY3rhj1ltz6xCI264E3oSXK5q1tdNIZ9cqinB-GZ-x7ieTPoVB1CF-ccAaypmFM8qFFnKSkyZaI3lxQW3o6B2kI~243p~X3~BDRBtRJ~2LCRkbJGmV1uIeLrkXJ9dXVSNWXCWueYSICw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
     button: 'View Live Video',
     link: 'true',
     date: "27 Oct",
@@ -19,7 +19,7 @@ var NewsData = [
     desc:
         "Ever been in a mess of faulty codes, scratching your brains to clear out the bugs ? Well we have someth..",
     image:
-        'https://s3-alpha-sig.figma.com/img/25da/736e/831c9e4847170e046c118db85f3c6e73?Expires=1666569600&Signature=hIPuRQoJ4dPlWfCINaryfSIqR2-RNDdB-d8riCbi3nG7Lyk4ljn2eCa2-NFRgOKHjOWoVm~t~mDGImg7Bvq76uNOuV~BZurX2YQa93Qo~C7ETqfwlbwXvnjGXkRw0~fcfWkh3gpz0zg1aw1e5YKQralLB~6H9ETtYp~Shfoi3Ah2LXXnjEp0XDj9TH8oVMkMtrM2j2ue8BsiiMHYs0aIzfBun8psjXbfk848vbHdxeQ3eOoscvB6rbCQ29WUMN-nUvr0k5Mf58BoS1X8lOsYV537m2R5lDwvGcziDye1LRO18KDjbP9A9ZzBNHA-pXCfNisGrCSDRUtuAtCFE-L4mg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+        'https://s3-alpha-sig.figma.com/img/25da/736e/831c9e4847170e046c118db85f3c6e73?Expires=1672617600&Signature=EixmCv6KJNdym94qYDq4A3VCHMEeo-TZncJYp~YAMxFub5Tj~nifbKuC8oW7ZSVkGkY6Dri4cEARXmIb0YHInwUb0jX9E3Vjbcpribp22bpaE1nTlPVMkP4gNLl0dypQHQ5rWss2GvL8Cxsx8Z08X5xdC7du~Is27g3eNqYusMviD4aBVT~SqfKWiXk1ZqyhqgMOI4ESINDvOlTFJvxFCjP-b47XM-SqOAy61Sq9EWWwGminBxhMmiGwXjEPyFhKUMjU2fWCc0kWk3b1JK4VWd8tqPeB-mYIYeooJc1CbYwc6M0riaT7zMaNruGJjy3hBN6BSv-Kc1gEtGgYmuPrSw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
     button: 'Register Now',
     link: 'true',
     date: "27 Oct",
@@ -31,7 +31,7 @@ var NewsData = [
     desc:
         "Ever been in a mess of faulty codes, scratching your brains to clear out the bugs ? Well we have someth..",
     image:
-        'https://s3-alpha-sig.figma.com/img/e9e4/d8c2/b1dfbb8af16328533da4aa51f080832d?Expires=1666569600&Signature=Fp9Ifyb~i2RR~Y~0RZX46PoIruo3SqwxWsMDAcB60MakHdsiFqNPQ07XTqx5pqWQJvDPgRalLOaClwarEY87ADPKLIlJoQcboHS9~IV7EaLV1OGKGfMSwFuK0rV4KB7xnM6SqiO7zmSwHD4aip8p17ToH5TWq5cZZlmxB9OEO99H4pGr5cA4~qschnXj6w1la41nJUDHX6BT2Wfh0MrsFAqBnr-ZJnAjvOgNoOQFa2nPWuBz4GCAaHUBRqSSI2NPxW7ADzpgQgxb7caC9BQbaguEIS0C5ZOPb2FBrMLmMj3FYV~hOXH7XFhLZP6Liv8nKlBtAlg3m3yJkId5aiNn-Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
+        'https://s3-alpha-sig.figma.com/img/e9e4/d8c2/b1dfbb8af16328533da4aa51f080832d?Expires=1672617600&Signature=MF6u6ZqbFUY4s8h217RP3S16ZbfG9ywfiyenMzLWOLak~PCkGFj3h1BozyW4342pNsWwkOg3DDUF4EjmE5dj2TyNXyEynbssi~aE4UnlQzNL4PhGbtz6qmNrbjkpqzIKnvSmXgRwJvhQFMNIyjToQgzPGXtMR3ZTbfdKPkiVfO52Z2RnwqciirM91AV75TU92Imt-3ooaY8sk98SkJiv8IrcYWIfNmK64lAN3E0~fUH7tl-kMJdTH8J04pSMtsTK3FYfpy797lsjCefQY0J5yxSaBpIz3F0BBtHXXLxzOdp8KIggu9PqH6GFIsl9MrYH-UDgyZz2FUiV1gPWoWiBBw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
     button: 'Register Now',
     link: 'true',
     date: "27 Oct",
