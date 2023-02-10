6be0fe4edfc7091eabd8331df20c2d9db81fb77c
Tom Vempala <thommuvtt123@gmail.com>
2023-02-10 15:50:26 +0530

    sthg

--- lib/Services/Notifications/firebase_messaging.dart
+++ lib/Services/Notifications/firebase_messaging.dart
@@ -1,25 +1,34 @@
-
+import 'package:excelapp/Services/Database/hive_operations.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:rxdart/rxdart.dart';
+
 // used to pass messages from event handler to the UI
 final _messageStreamController = BehaviorSubject<RemoteMessage>();
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
- await Firebase.initializeApp();
+  await Firebase.initializeApp();
 
- if (kDebugMode) {
-   print("Handling a background message: ${message.messageId}");
-   print('Message data: ${message.data}');
-   print('Message notification: ${message.notification?.title}');
-   print('Message notification: ${message.notification?.body}');
- }
+  if (kDebugMode) {
+    print("Handling a background message: ${message.messageId}");
+    print('Message data: ${message.data}');
+    print('Message notification: ${message.notification?.title}');
+    print('Message notification: ${message.notification?.body}');
+    List notifications = await HiveDB.retrieveData(valueName: 'notifications')??[];
+    final newNotification = {
+      'data' : message.data,
+      'title':'${message.notification?.title}',
+      'body': '${message.notification?.body}'
+    };
+    notifications.add(newNotification);
+    HiveDB.storeData(valueName: 'notifications',value: newNotification);
+  }
 }
 
-void initiliaseNotificationServices() async{
+void initiliaseNotificationServices() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -38,24 +47,30 @@ void initiliaseNotificationServices() async{
 
 //   if (kDebugMode) {
 //     print('Permission granted: ${settings.authorizationStatus}');
-//   } 
+//   }
 //   String token = await messaging.getToken();
 
 // if (kDebugMode) {
 //   print('Registration Token=$token');
 // }
-   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-   if (kDebugMode) {
-     print('Handling a foreground message: ${message.messageId}');
-     print('Message data: ${message.data}');
-     print('Message notification: ${message.notification?.title}');
-     print('Message notification: ${message.notification?.body}');
-   }
 
-   _messageStreamController.sink.add(message);
- });
-  // TODO: Set up background message handler
-   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    if (kDebugMode) {
+      print('Handling a foreground message: ${message.messageId}');
+      print('Message data: ${message.data}');
+      print('Message notification: ${message.notification?.title}');
+      print('Message notification: ${message.notification?.body}');
+    }
+    void _handleMessage(RemoteMessage message) {
+      if (message.data['event'] == 'chat') {
+        // Navigator.push(context, route);
+      }
+    }
 
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
 
+    _messageStreamController.sink.add(message);
+  });
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 }
