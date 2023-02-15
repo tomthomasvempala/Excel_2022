113bcd62d17667cca4753bb250fe429cff1006ad
Tom Vempala <thommuvtt123@gmail.com>
2023-02-15 18:15:21 +0530

    latest news removed

--- lib/UI/Screens/HomePage/homePage.dart
+++ lib/UI/Screens/HomePage/homePage.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
               ),
 
               Discover(),
-              LatestNewsSection(),
+              // LatestNewsSection(),
               // Categories(),
               // SocialIcons(),
             ],
