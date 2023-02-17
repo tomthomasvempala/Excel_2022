cd99f51ca63b957263d2730316715a76767fcd7f
Faheem <faheempp@outlook.com>
2023-02-17 16:09:54 +0530

    eventdetails- contact tab update

--- lib/UI/Screens/EventPage/Widgets/eventDetails.dart
+++ lib/UI/Screens/EventPage/Widgets/eventDetails.dart
@@ -5,11 +5,12 @@ import 'package:excelapp/Models/event_details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:html/dom.dart' as dom;
-
+import 'package:url_launcher/url_launcher.dart';
 import '../../../constants.dart';
 
 class MoreEventDetails extends StatefulWidget {
   final EventDetails eventDetails;
+
   MoreEventDetails({this.eventDetails});
 
   @override
@@ -99,15 +100,27 @@ class _MoreEventDetailsState extends State<MoreEventDetails> {
           ? widget.eventDetails.format.replaceAll('<h2/>', "</h2>")
           : "No event format";
     else if (pageNumber == 3)
-      content = widget.eventDetails.rules ??
-          "Rules are not specified for this event";
-    else if (pageNumber == 4)
-    {
-      var eventhead1 =json.decode(widget.eventDetails.eventHead1);
-      var eventhead2 =json.decode(widget.eventDetails.eventHead2);
-      String head1 = "<h3> ${eventhead1['name']}</h3>\n<p>Ph:  ${eventhead1['phoneNumber']}</p>\n<p>Email: ${eventhead1['email']}</p><br>\n";
-      String head2 = "<h3> ${eventhead2['name']}</h3>\n<p>Ph:  ${eventhead2['phoneNumber']}</p>\n<p>Email: ${eventhead2['email']}</p>";
-    content = head1 + head2;
+      content =
+          widget.eventDetails.rules ?? "Rules are not specified for this event";
+    else if (pageNumber == 4) {
+      var eventhead1 = json.decode(widget.eventDetails.eventHead1);
+      var eventhead2 = json.decode(widget.eventDetails.eventHead2);
+      String head1 =
+          "<h3> ${eventhead1['name']}</h3>\n<p>Ph:  ${eventhead1['phoneNumber']}</p>\n<p>Email: ${eventhead1['email']}</p><br>\n";
+      String head2 =
+          "<h3> ${eventhead2['name']}</h3>\n<p>Ph:  ${eventhead2['phoneNumber']}</p>\n<p>Email: ${eventhead2['email']}</p>";
+      content = head1 + head2;
+      return Container(
+        color: Colors.white,
+        //padding: padding,
+        child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 0, horizontal: 18),
+          children: [
+            eventHeadDetails(eventhead1['name'], eventhead1['phoneNumber'], eventhead1['email']),
+            eventHeadDetails(eventhead2['name'], eventhead2['phoneNumber'], eventhead2['email']),
+          ],
+        ),
+      );
     }
     return Container(
       decoration: BoxDecoration(color: Colors.white),
@@ -160,6 +173,78 @@ class _MoreEventDetailsState extends State<MoreEventDetails> {
                   fontSize: 14.5,
                   fontWeight: FontWeight.w400),
             )),
+      ),
+    );
+  }
+
+  Widget eventHeadDetails(String eventHeadName,String eventHeadNumber,String eventHeadEmail){
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 6),
+      width: MediaQuery.of(context).size.width * 1,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(eventHeadName,
+                  style: TextStyle(
+                    fontFamily: pfontFamily,
+                    fontSize: 16,
+                    height: 1.3,
+                    color: Color(0xff3D4747),
+                    fontWeight: FontWeight.w700,
+                  )),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                  eventHeadNumber,
+                  style: TextStyle(
+                      fontFamily: pfontFamily,
+                      fontSize: 14,
+                      height: 1.5,
+                      color: Color(0xff3D4747))
+              ),
+            ],
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(onPressed: ()async{
+                  final Uri launchUri = Uri(
+                    scheme: 'tel',
+                    path: eventHeadNumber,
+                  );
+                  await launchUrl(launchUri);
+                }, icon: Icon(Icons.phone_in_talk_rounded),color: Colors.blue,iconSize: 28,),
+                IconButton(onPressed: ()async{
+                  Uri launchUri = Uri(
+                    scheme: 'mailto',
+                    path: eventHeadEmail,
+                  );
+                  if(await canLaunchUrl(launchUri) != null){
+                    await launchUrl(launchUri);
+                  }
+                  else{
+                    throw 'Could not launch';
+                  }
+
+
+                }, icon: Icon(Icons.mail_outline_rounded),color: Colors.blue,iconSize: 28),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
